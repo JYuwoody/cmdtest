1161ccef2a7d4dd8edfb60812e953f9435e56895
JYuwoody <zx0935697850@gmail.com>
2022-06-11 13:53:19 +0800

    tesr PointString.c

--- PointString.c
+++ PointString.c
@@ -7,6 +7,9 @@ void main()
     char name[32];
     char *names[5];
     int count = 0;
+    //woody >>>
+    int test1 = 0;
+    //woody <<<
 
     for(count=0;count<5;count++)
     {
@@ -15,6 +18,10 @@ void main()
         names[count] = (char*) malloc(strlen(name)+1);
         strcpy(names[count],name);
     }
+    //woody >>>
+    printf("test1 = %d: ", test1);
+    //woody <<<
+
     for(count=0;count<5;count++)
     {
         printf("%s   %p\n",names[count],names[count]);
